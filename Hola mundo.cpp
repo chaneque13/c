c2924c5a61e9c436cd7e10dc6e1e784ebeeafa12
Behemoth <115022458+chaneque13@users.noreply.github.com>
2023-01-23 14:40:33 -0600

    Add files via upload

---
+++ Hola mundo.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h> 
+//esta libreria es vital. el .h significa que es un header, un archivo de cabecera
+//el stdio, standart input output, junto con el .h seria, standart input output header, nos sirve para poder poner cosas en pantalla y que el usuario pueda digitar datos
+
+
+
+//int es una funcion integer, entera, 
+int main(){ //funcion principal 
+
+			//funciona para imprimir en pantalla 
+			printf("Hola mundo\n");
+
+				//finaliza el programa, no es necesario
+			return 0;
+}
+
+
+
+
+
