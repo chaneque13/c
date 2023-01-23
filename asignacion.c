e98ac156eaf0e697ef300cd0e48b2b9937c453c6
Behemoth <115022458+chaneque13@users.noreply.github.com>
2023-01-23 15:58:42 -0600

    Add files via upload

---
+++ asignacion.c
@@ -0,0 +1,20 @@
+//Operadores de Asignacion
+
+
+#include<stdio.h>
+
+
+int main(){
+	int a,b,c;
+	a=20;
+	//a += 10,	//a=a+10	
+	//a -=5; //a=a-10
+	//a *=3; //a=a*3
+	a /= 4; //a=a/2
+
+
+	printf("El valor de a: %i",a);
+	
+	
+	return 0;
+}
